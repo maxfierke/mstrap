664074e5367d26bec3e0713424136480a42aae4c
Max Fierke <max@maxfierke.com>
2019-02-04 23:39:16 -0600

    Add docker-compose wrapper step

---
+++ src/mstrap/steps/compose_step.cr
@@ -0,0 +1,24 @@
+module MStrap
+  module Steps
+    class ComposeStep < Step
+      include Utils::Logging
+      include Utils::System
+
+      def bootstrap
+        ensure_docker!
+        Process.exec("docker-compose", ["-f", MStrap::Paths::SERVICES_YML] + args)
+      end
+
+      private def args
+        options[:step_args].as?(Array(String)) || [] of String
+      end
+
+      private def ensure_docker!
+        unless cmd "docker-compose version"
+          logw "Could not find 'docker-compose'."
+          logc "Please ensure you've done a full run of mstrap and docker has been installed"
+        end
+      end
+    end
+  end
+end

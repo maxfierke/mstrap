0f367eae45e6c81e7919150e89475a18da74cf75
Max Fierke <max@maxfierke.com>
2019-10-19 11:33:25 -0500

    Implement support for managed profile revisions

--- src/mstrap/profile_fetcher.cr
+++ src/mstrap/profile_fetcher.cr
@@ -2,6 +2,11 @@ module MStrap
   class ProfileFetcher
     class ProfileFetchError < Exception; end
     class InvalidProfileUrlError < ProfileFetchError; end
+    class GitProfileFetchError < ProfileFetchError
+      def initialize(config)
+        super("#{config.name}: Could not clone profile via git. Ensure you have access.")
+      end
+    end
 
     include Utils::System
 
@@ -35,10 +40,10 @@ module MStrap
           )
         end
       elsif git_url?(url)
-        unless cmd("git", "clone", url.to_s, config.dir, quiet: true)
-          raise ProfileFetchError.new(
-            "#{config.name}: Could not clone profile via git. Ensure you have access."
-          )
+        if File.directory?(config.dir)
+          update_profile_from_git!
+        else
+          git_clone_profile!(url)
         end
       elsif url.scheme == "https"
         ensure_profile_dir
@@ -54,6 +59,24 @@ module MStrap
       self
     end
 
+    private def git_clone_profile!(url)
+      unless cmd("git", "clone", url.to_s, config.dir, quiet: true)
+        raise GitProfileFetchError.new(config)
+      end
+
+      if config.revision
+        git_checkout_profile_revision!
+      end
+    end
+
+    private def git_checkout_profile_revision!
+      Dir.cd(config.dir) do
+        unless cmd("git", "checkout", config.revision.not_nil!, quiet: true)
+          ProfileFetchError.new("#{config.name}:  could not checkout revision #{config.revision}")
+        end
+      end
+    end
+
     private def ensure_profile_dir
       Dir.mkdir_p(config.dir, mode: 0o500)
     end
@@ -64,13 +87,51 @@ module MStrap
         (!url.scheme || url.scheme == "https") && url.path.ends_with?(".git")
     end
 
+    private def https_url?(url)
+      url.scheme == "https" && !url.path.ends_with?(".git")
+    end
+
     private def should_update?
       force? || outdated_revision?
     end
 
     private def outdated_revision?
-      # TODO: Implement
-      false
+      revision = config.revision
+
+      if !revision
+        return nil
+      end
+
+      url = URI.parse(config.url)
+      url.normalize!
+
+      if git_url?(url) && File.directory?(config.dir)
+        Dir.cd(config.dir) do
+          `git rev-parse HEAD`.chomp != `git rev-parse #{revision}`.chomp
+        end
+      elsif https_url?(url) && File.exists?(config.path)
+        algo, hsh = revision.split(":")
+        digester = OpenSSL::Digest.new(algo.upcase)
+        digester.file(config.path).hexdigest != hsh
+      else
+        false
+      end
+    end
+
+    private def update_profile_from_git!
+      Dir.cd(config.dir) do
+        if config.revision
+          unless cmd("git", "fetch", "origin", quiet: true)
+            raise GitProfileFetchError.new(config)
+          end
+
+          git_checkout_profile_revision!
+        else
+          unless cmd("git", "pull", "origin", "master", quiet: true)
+            raise GitProfileFetchError.new(config)
+          end
+        end
+      end
     end
   end
 end

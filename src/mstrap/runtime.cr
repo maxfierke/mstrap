2ba386daad1306b899bbebb582c0efd35620dc6b
Max Fierke <max@maxfierke.com>
2023-02-28 20:29:14 -0600

    runtime: address ameba violation

--- src/mstrap/runtime.cr
+++ src/mstrap/runtime.cr
@@ -65,7 +65,7 @@ module MStrap
       `asdf list #{asdf_plugin_name} 2>&1`
         .chomp
         .split("\n")
-        .map { |version| version.strip.lstrip('*') }
+        .map(&.strip.lstrip('*'))
         .reject do |version|
           version.blank? || version == "No versions installed"
         end

4d31b5f1f26e21e8bd8606075ac7b3fc4927845c
Max Fierke <max@maxfierke.com>
2024-05-08 20:00:26 -0500

    Remove xcrun lookup of git
    xcrun is also a shim, and because we're optimizing for strap.sh & Homebrew, they'll install Xcode CLT anyway. Therefore, there's little value here in trying to be extra robust by using xcrun

--- src/mstrap/platform/darwin/macos.cr
+++ src/mstrap/platform/darwin/macos.cr
@@ -9,9 +9,9 @@ module MStrap
 
       # Indicates whether the host platform has Git installed
       #
-      # NOTE: On macOS, there's a few tricks involved due to Xcode shims, so we'll only
-      # consider it installed if it's from Homebrew (or elswhere),
-      # accessible via `xcrun` or installed via Xcode Command Line Tools
+      # NOTE: On macOS, there's a few tricks involved due to Xcode shims, so
+      # we'll only consider it installed if it's from Homebrew (or elsewhere),
+      # or installed via Xcode Command Line Tools
       def self.has_git?
         git_path = @@git_path
         return true if git_path != ""
@@ -20,14 +20,7 @@ module MStrap
 
         # Ignore the Xcode CLT shim trickery!
         if git_path && git_path == "/usr/bin/git"
-          # Try and look it up with xcrun
-          if has_command?("xcrun")
-            xcrun_git_path = `xcrun -find git 2>/dev/null`.chomp
-            git_path = Process.find_executable(xcrun_git_path) if $?.success?
-          end
-
-          # Fallback to default CLT path
-          git_path ||= Process.find_executable(XCODE_CLT_GIT_PATH)
+          git_path = Process.find_executable(XCODE_CLT_GIT_PATH)
         end
 
         return false if git_path.nil?
